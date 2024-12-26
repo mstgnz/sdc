b4bc79433eecb239bc7f079173692685d70e376b
Mesut GENEZ <genezmesut@gmail.com>
2024-12-26 23:12:05 +0300

    example

--- example/main.go
+++ example/main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/mstgnz/gosql/convert"
-)
-
 func main() {
-	if statement, err := gosql.Cleaner("example/files/mysql.sql"); err != nil {
-		fmt.Println("Error: ", err)
-	} else {
-		for _, stmt := range gosql.Parser(statement) {
-			fmt.Println(stmt)
-		}
-	}
+	// TODO example/files dizini içersindeki dosyalar parser ile parse edilecek. örnek kodları ekleyeceğiz
 }
